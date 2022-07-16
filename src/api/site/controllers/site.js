c1e9ddce5c2072a212d7653c2c6752edf6b7778d
Mulelur <63222158+Mulelur@users.noreply.github.com>
2022-07-16 09:38:36 +0200

    site pages

--- src/api/site/controllers/site.js
+++ src/api/site/controllers/site.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  project controller
+ *  site controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::project.project');
+module.exports = createCoreController('api::site.site');
--- src/api/site/services/site.js
+++ src/api/site/services/site.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * project service.
+ * site service.
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::project.project');
+module.exports = createCoreService('api::site.site');
